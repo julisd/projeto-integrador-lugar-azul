72ef7833b7b582011af3a9571416b0ec8ab9f3a1
Juliana <julisd3@gmail.com>
2024-06-08 15:44:48 -0300

    style

--- resources/views/pessoa/home.blade.php
+++ resources/views/pessoa/home.blade.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Buscar Estabelecimentos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
