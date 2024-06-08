995ca8cb59dbee23888948616fec16244c63892d
Juliana <julisd3@gmail.com>
2024-06-08 15:12:52 -0300

    style

--- resources/views/pessoa/home.blade.php
+++ resources/views/pessoa/home.blade.php
@@ -240,7 +240,7 @@
 
 
                     const listItem = document.createElement('div');
-                    listItem.classList.add('card', 'mb-3','estabelecimento-card');
+                    listItem.classList.add('estabelecimento-card');
                     const image = document.createElement('img');
                     const imageSrc = imageBasePath + '/' + estabelecimento.image;
                     image.src = imageSrc;
